617733edc75e90eed96c66573ac1eb5f6401ca3e
Francisco Alvarez <francisco.alvarez@alumnos.udg.mx>
2016-10-04 18:22:43 -0500

    Se agregó el método set_atributo

--- t06.py
+++ t06.py
@@ -3,3 +3,6 @@ class Tarea06():
     def __init__(self):
         self.atributo = 0
         self.noEsAtributo = "No soy atributo :D"
+
+    def set_Atributo(self,valor):
+        self.atributo = valor
